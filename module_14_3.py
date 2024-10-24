2bd94261d28a246c782bcf7ff374f5a964fdd17d
andreyalf1961 <bazulin.a.i@gmail.com>
2024-10-24 11:16:57 +0300

    Update module_14_3.py

--- module_14_3.py
+++ module_14_3.py
@@ -66,7 +66,7 @@ async def get_buying_list(message):
 @dp.callback_query_handler(text='product_buying')
 async def send_confirm_message(call):
     await call.message.answer('Вы успешно приобрели продукт!')
-
+    await call.answer()
 
 @dp.callback_query_handler(text='formulas')
 async def get_formulas(call):
